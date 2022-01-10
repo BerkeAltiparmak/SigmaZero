97294918877b93498be9a5c4bbfe07443b78f67f
Boran Erol <boranerol@Borans-MacBook-Air.local>
2022-01-10 20:20:51 +0300

    lutfen ol

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -2,6 +2,11 @@ import pygame as p
 
 import ChessEngine
 
+<<<<<<< HEAD
+=======
+# deneme 1 2
+
+>>>>>>> parent of e3e77df (Boran Deneme)
 GRID_WIDTH = GRID_HEIGHT = 512
 WIDTH = HEIGHT = GRID_WIDTH + 100
 DIMENSION = 8
